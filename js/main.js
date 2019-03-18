02c7759ae85a904b8910f05bc826d51d023ef351
Kseniya Kholmogorova <mademoiselle.alisa@gmail.com>
2019-03-18 22:40:51 +0300

    Последние правки

--- js/main.js
+++ js/main.js
@@ -7,3 +7,16 @@
     bookingForm.classList.toggle("form-show");
   }
   );
+
+  var formBooking = document.querySelector(".search-hotel");
+  var dateIn = formBooking.querySelector("[name=date-in]");
+  var dateOut = formBooking.querySelector("[name=date-out]");
+
+  formBooking.addEventListener("submit", function (evt) {
+    if ( dateIn.value == '' || dateOut.value == '' ) {
+      evt.preventDefault();
+      formBooking.classList.remove("modal-error");
+      formBooking.offsetWidth = formBooking.offsetWidth;
+      formBooking.classList.add("modal-error");
+    }
+  });
